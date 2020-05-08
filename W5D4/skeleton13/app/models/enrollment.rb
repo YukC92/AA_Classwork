c8508648734ac9c631204324671519d72d63a8f7
yuk fai chan <yukfaichan92@gmail.com>
2020-05-07 22:00:16 -0700

    W5D4 Classwork

---
+++ W5D4/skeleton13/app/models/enrollment.rb
@@ -0,0 +1,14 @@
+class Enrollment < ApplicationRecord
+
+    belongs_to(:user,
+    { primary_key: :id,
+      foreign_key: :user_id,
+      class_name: :User
+    })
+
+    has_many(:course,
+    {   primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
+    })
+end
